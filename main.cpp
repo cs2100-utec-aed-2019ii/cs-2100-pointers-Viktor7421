2a188a856a1d1a743747e0185e772e8aa1419985
Victor <victor.ascuna@gmail.com>
2019-08-22 17:46:43 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,46 @@ void ejercicio_x (int, int) {
 }
 
 int main (int, char * []){
+    // 1)  1 2 3 4 5 6 7 8 9 10
+    // 2)  7 5 10
+    // 3)  T
+    //     L
+    //     T
+    // 4)  *p = 45
+    // 5)  SI, porque a es un char y p esta preparado para recibir un double.
+    // 6)  void swap (int* x, int* y){
+    //          int temp = *x; 
+    //          *x = *y;   
+    //          *y = temp;
+    //     }
+    // 7)  2 8
+    // 8)  0
+    //     8
+    // 9)  8
+    //     15
+    // 10) 8
+    // 11) void func(int a, int* ptr){
+    //          ptr = &a;
+    //          cout << a << ptr << &ptr;
+    //     }
+    // 12) 13 13 13
+    // 13) 5 10 7
+    // 14) 4 1
+    // 15) int a = 1;
+    //     int b = 2;
+    //     int *ptrA;
+    //     int *ptrB;
+    //     int s;
+    //     ptrA = &a;
+    //     ptrB = &b;
+    //     s = *ptrA + *ptrB;
+    // 16) 19
+    // 17) ERROR
+    // 18) 18
+    // 19)
+    // 20)
+    
+    
     return 1;
 }
+
